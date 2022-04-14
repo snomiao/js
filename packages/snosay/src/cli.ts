28a0579a024646cbdb279023dd86b2e3e4668df6
snomiao <snomiao@gmail.com>
2022-04-14 22:23:17 +0800

    chore: format tsconfig

--- packages/snosay/src/cli.ts
+++ packages/snosay/src/cli.ts
@@ -1,44 +1,43 @@
-import arg from "arg";
-import { resolve } from "path";
-import { exportFile, getInstalledVoices, speak } from "./say";
-
+import arg from 'arg';
+import { resolve } from 'path';
+import { exportFile, getInstalledVoices, speak } from './say';
 
 function parseArgumentsIntoOptions(rawArgs: string[]) {
   const opts = {
-    "--voice": String,
-    "--speed": Number,
-    "--list": Boolean,
-    "--output": String,
-    "-l": "--list",
-    "-v": "--voice",
-    "-s": "--speed",
-    "-o": "--output",
+    '--voice': String,
+    '--speed': Number,
+    '--list': Boolean,
+    '--output': String,
+    '-l': '--list',
+    '-v': '--voice',
+    '-s': '--speed',
+    '-o': '--output',
   };
   const args = arg(opts, {
     argv: rawArgs.slice(2),
   });
   return {
     text: args._.join(' '),
-    speed: args["--speed"] || undefined,
-    voice: args["--voice"] || undefined,
-    list: args["--list"] || undefined,
-    output: args["--output"] || undefined,
+    speed: args['--speed'] || undefined,
+    voice: args['--voice'] || undefined,
+    list: args['--list'] || undefined,
+    output: args['--output'] || undefined,
   };
 }
 
-
 export default cli;
 export async function cli(args: string[]) {
-  const { text, voice, speed, output, list } =
-    parseArgumentsIntoOptions(args);
+  const {
+    text, voice, speed, output, list,
+  } = parseArgumentsIntoOptions(args);
   if (list) {
     const voicelist = await getInstalledVoices();
-    console.log(voicelist.join("\n"));
+    console.log(voicelist.join('\n'));
     return;
   }
   if (!text) {
-    console.error('ERROR: text is required')
-    return
+    console.error('ERROR: text is required');
+    return;
   }
   if (output) {
     const path = resolve(output);
