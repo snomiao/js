b77342e40c1891e43e91c78358ed8da3eec23fa6
snomiao <snomiao@gmail.com>
2022-04-15 12:36:03 +0800

    fix: console.log(text);

--- packages/snosay/src/cli.ts
+++ packages/snosay/src/cli.ts
@@ -1,42 +1,40 @@
-import arg from 'arg';
-import { resolve } from 'path';
-import { exportFile, getInstalledVoices, speak } from './say';
+import arg from "arg";
+import { resolve } from "path";
+import { exportFile, getInstalledVoices, speak } from "./say";
 
 function parseArgumentsIntoOptions(rawArgs: string[]) {
   const opts = {
-    '--voice': String,
-    '--speed': Number,
-    '--list': Boolean,
-    '--output': String,
-    '-l': '--list',
-    '-v': '--voice',
-    '-s': '--speed',
-    '-o': '--output',
+    "--voice": String,
+    "--speed": Number,
+    "--list": Boolean,
+    "--output": String,
+    "-l": "--list",
+    "-v": "--voice",
+    "-s": "--speed",
+    "-o": "--output",
   };
   const args = arg(opts, {
     argv: rawArgs.slice(2),
   });
   return {
-    text: args._.join(' '),
-    speed: args['--speed'] || undefined,
-    voice: args['--voice'] || undefined,
-    list: args['--list'] || undefined,
-    output: args['--output'] || undefined,
+    text: args._.join(" "),
+    speed: args["--speed"] || undefined,
+    voice: args["--voice"] || undefined,
+    list: args["--list"] || undefined,
+    output: args["--output"] || undefined,
   };
 }
 
 export default cli;
 export async function cli(args: string[]) {
-  const {
-    text, voice, speed, output, list,
-  } = parseArgumentsIntoOptions(args);
+  const { text, voice, speed, output, list } = parseArgumentsIntoOptions(args);
   if (list) {
     const voicelist = await getInstalledVoices();
-    console.log(voicelist.join('\n'));
+    console.log(voicelist.join("\n"));
     return;
   }
   if (!text) {
-    console.error('ERROR: text is required');
+    console.error("ERROR: text is required");
     return;
   }
   if (output) {
@@ -45,5 +43,6 @@ export async function cli(args: string[]) {
     console.log(path);
     return;
   }
+  console.log(text);
   await speak(text, voice, speed);
 }
