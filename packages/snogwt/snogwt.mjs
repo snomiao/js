c77caf578280d7f815d624425826bd3ed889c5f6
snomiao <snomiao@gmail.com>
2022-05-03 09:42:52 +0800

    fix: typo

--- packages/snogwt/snogwt.mjs
+++ packages/snogwt/snogwt.mjs
@@ -123,7 +123,7 @@ async function versionShow(js) {
 async function tryInstallPackages(checkoutPath) {
   if (0) {
   } else if (`${checkoutPath}/pnpm-lock.yaml`)
-    await execSh.promise(`cd ${checkoutPath} && pnpm i)`).catch(() => null);
+    await execSh.promise(`cd ${checkoutPath} && pnpm i`).catch(() => null);
   else if (`${checkoutPath}/yarn.lock`)
     await execSh.promise(`cd ${checkoutPath} && yarn`).catch(() => null);
   else if (`${checkoutPath}/package-lock.json`)
