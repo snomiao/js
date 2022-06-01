c9dbfbcbe336619afb2db2b6d2ceb7c1fdd6123e
snomiao <snomiao@gmail.com>
2022-06-01 20:23:51 +0800

    chore: fix params

--- packages/snobuild/src/cli.ts
+++ packages/snobuild/src/cli.ts
@@ -3,22 +3,27 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import snobuild from "./index";
 
-await snobuild(
-  await yargs(hideBin(process.argv))
-    .scriptName("snobuild")
-    .usage("snobuild")
-    .help()
-    .version()
-    .boolean("init")
-    .default("init", false, "initialize package.json")
-    .string("outdir")
-    .default("outdir", "lib", "output dir")
-    .boolean("watch")
-    .default("watch", false, "watch mode")
-    .strict()
-    .alias("i", "init")
-    .alias("o", "outdir")
-    .alias("w", "watch")
-    .alias("h", "help")
-    .alias("v", "version").argv,
-);
+const argv = await yargs(hideBin(process.argv))
+  .alias("i", "init")
+  .alias("o", "outdir")
+  .alias("w", "watch")
+  .alias("h", "help")
+  .alias("v", "version")
+  .scriptName("snobuild")
+  .usage("snobuild")
+  .help()
+  .version()
+  .boolean("init")
+  .default("init", false)
+  .describe("init", "initialize package.json")
+  //
+  .string("outdir")
+  .default("outdir", "lib")
+  .describe("outdir", "output dir")
+  //
+  .boolean("watch")
+  .default("watch", false)
+  .describe("watch", "watch mode")
+  //
+  .strict().argv;
+await snobuild(argv);
