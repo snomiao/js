a3c31013cc2e9dd140ea68265211ec5bb6a4985b
snomiao <snomiao@gmail.com>
2022-06-01 21:10:21 +0800

    chore: use snobuild

--- packages/sno-utils/src/index.ts
+++ packages/sno-utils/src/index.ts
@@ -1,5 +1,4 @@
 /**
- * imports...
  * author: YiDong Zhuo(snomiao@gmail.com)
  */
 
@@ -11,10 +10,3 @@ export * from "./测试";
 export * from "./类型";
 export * from "./统计";
 export * from "./表列";
-
-// this line do nothing but ensure it's an es-module
-// await Promise.resolve()
-
-// if (require.main === module) (async () => {
-//     console.log('你好, 世界');
-// })().then(console.log).catch(console.error)
