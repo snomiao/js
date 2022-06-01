8e5cb48abc2040559b447db3acc1d1701c9a1d5c
snomiao <snomiao@gmail.com>
2022-06-01 16:42:06 +0800

    chore: use snobuild

---
+++ packages/chrome-dev/src/index.ts
@@ -0,0 +1,54 @@
+import { resolve } from "path";
+import puppeteer from "puppeteer";
+/**
+ * chrome-dev
+ * Author: snomiao <snomiao@gmail.com>
+ */
+export default async function chromeDev({
+  _ = [] as (string | number)[],
+  extension = "",
+  devtools = false,
+  ignoreHTTPSErrors = true,
+  ignoreDefaultArgs = false,
+  port = 0,
+  dotenv = true,
+  $0 = "chrome-dev",
+  ...otherArgs
+} = {}) {
+  console.log("otherArgs=", otherArgs);
+
+  // open urls
+  const urls = new Set(_.map(String));
+
+  // TODO: match PORT=80, HOST=localhost, URL=localhost:80 and parse into URL
+  if (port) urls.add(`https://localhost:${port}/`);
+
+  const chromeArgs = [];
+  // load extension
+  if (extension) {
+    const extensionPath = resolve(process.cwd(), extension);
+    chromeArgs.push(`--load-extension=${extensionPath}`);
+    urls.add("chrome://extensions");
+  }
+  if (dotenv) {
+    await import("dotenv/config");
+  }
+  // launch browser
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools,
+    ignoreDefaultArgs: ignoreDefaultArgs ? true : ["--disable-extensions"],
+    args: [...chromeArgs],
+    defaultViewport: null,
+    ignoreHTTPSErrors,
+  });
+
+  // need more page
+  if (urls.size) console.log("open urls: ", [...urls.values()]);
+  while ((await browser.pages()).length < urls.size) await browser.newPage();
+
+  const pages = await browser.pages();
+  await Promise.allSettled(pages.map((page, i) => page.goto([...urls.values()][i])));
+
+  console.log("All pages opened");
+}
