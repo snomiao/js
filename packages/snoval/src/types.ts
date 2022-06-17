fdf973cd017663703b84955eb2981f047faa7707
snomiao <snomiao@gmail.com>
2022-06-18 00:05:01 +0800

    chore: remove snoval

--- packages/snoval/src/types.ts
+++
@@ -1,5 +0,0 @@
-export type PromiseLiker<T> = PromiseLike<T> | T;
-export type Iterater<V, R> = { (v: V, i: number, a: V[]): PromiseLiker<R> };
-export type Reducer<V, R> = { (r: R, v: V, i: number, a: V[]): PromiseLiker<R> };
-export type Looper<V> = { (v: V, i: number): PromiseLiker<void> };
-export type Conder<R> = { (): PromiseLiker<R> };
