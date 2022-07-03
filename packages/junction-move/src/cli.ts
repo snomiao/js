1803d11f3454385776692cbb241d851927165008
snomiao <snomiao@gmail.com>
2022-07-03 15:41:58 +0800

    fix(junction-move): upgrade snobuild

--- packages/junction-move/src/cli.ts
+++ packages/junction-move/src/cli.ts
@@ -2,12 +2,19 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import junctionMove from "./index";
-const argv = await yargs(hideBin(process.argv))
-  .usage("Usage: npx junction-move source_folder target_folder")
-  .example("$0", "C:\\Go D:\\Go")
-  .help("h")
-  .alias("h", "help")
-  .demandCommand(2, 2)
-  .epilog("Copyright (c) 2020 snomiao@gmail.com").argv;
-const [source, target] = argv._.map(String);
-await junctionMove(source, target);
+
+(async function () {
+  const argv = await yargs(hideBin(process.argv))
+    // .usage("Usage: npx junction-move source_folder target_folder")
+    .scriptName("junction-move")
+    .example("$0", "C:\\Go D:\\Go")
+    .command("$0 <source> <target>")
+    .string("source")
+    .string("target")
+    .demandCommand(2, 2)
+    .alias("h", "help")
+    .alias("v", "version")
+    .epilog("Copyright (c) 2020-2022 snomiao@gmail.com").argv;
+  // const [source, target] = argv._.map(String);
+  await junctionMove(argv.source, argv.target);
+})();
