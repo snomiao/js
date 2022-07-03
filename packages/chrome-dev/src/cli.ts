a88d1c6dcb67d26d6098713c7b7c9731a0256d97
snomiao <snomiao@gmail.com>
2022-07-03 14:30:45 +0800

    fix(chrome-dev): no-top-level-await

--- packages/chrome-dev/src/cli.ts
+++ packages/chrome-dev/src/cli.ts
@@ -3,23 +3,31 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import chromeDev from "./index";
 
-const argv = await yargs(hideBin(process.argv))
-  .scriptName("chrome-dev")
-  .string("extension")
-  .describe("extension", "extension mode")
-  .number("port")
-  .describe("port", "open http://localhost:port")
-  .boolean("dotenv")
-  .describe("dotenv", "load .env setting")
-  .boolean("devtools")
-  .describe("devtools", "open devtools everypage")
-  // .strict()
-  .alias("d", "devtools")
-  .alias("e", "extension")
-  .alias("p", "port")
-  .alias("h", "help")
-  .alias("v", "version")
-  .help()
-  .version()
-  .epilog("(C) snomiao.com 2022").argv;
-await chromeDev({ ...argv, inputs: argv._.map(String) });
+cli();
+
+/**
+ *
+ * @author: snomiao <snomiao@gmail.com>
+ */
+export default async function cli() {
+  const argv = await yargs(hideBin(process.argv))
+    .scriptName("chrome-dev")
+    .string("extension")
+    .describe("extension", "extension mode")
+    .number("port")
+    .describe("port", "open http://localhost:port")
+    .boolean("dotenv")
+    .describe("dotenv", "load .env setting")
+    .boolean("devtools")
+    .describe("devtools", "open devtools everypage")
+    // .strict()
+    .alias("d", "devtools")
+    .alias("e", "extension")
+    .alias("p", "port")
+    .alias("h", "help")
+    .alias("v", "version")
+    .help()
+    .version()
+    .epilog("(C) snomiao.com 2022").argv;
+  await chromeDev({ ...argv, inputs: argv._.map(String) });
+}
