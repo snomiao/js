afda87abe7790c3393db645c55ac89c2b7373c88
snomiao <snomiao@gmail.com>
2022-07-03 14:50:24 +0800

    fix(snosay): wrap with aiife

--- packages/snosay/src/cli.ts
+++ packages/snosay/src/cli.ts
@@ -1,94 +1,32 @@
 #!/usr/bin/env node
-import { resolve } from "path";
-import { stdin, stdout } from "process";
-import * as readline from "readline";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import { exportFile, getInstalledVoices, speak } from ".";
+import snosay from "./index";
 
-const argv = await yargs(hideBin(process.argv))
-  .scriptName("snosay")
-  .string("voice")
-  .describe("voice", "voice engine, such as: Microsoft David Desktop")
-  .number("speed")
-  .describe("speed", "voice speed, base is 1, higher faster")
-  .boolean("list")
-  .describe("list", "get voice engine list")
-  .string("output")
-  .describe("output", "output file ")
-  // examples
-  .example("$0", "hello, world")
-  .example("$0", "--list")
-  .example("$0", '--voice "Microsoft David Desktop" --speed 0.5 hello, world')
-  .example("$0", '--voice "Microsoft Zira Desktop" --speed 1.5 hello, world')
-  .example("$0", '--voice "Microsoft Zira Desktop" --speed 1.5 --output ./')
-  // .string("domain")
-  .alias("h", "help")
-  .alias("l", "list")
-  .alias("v", "voice")
-  .alias("s", "speed")
-  .alias("o", "output").argv;
+(async function () {
+  const argv = await yargs(hideBin(process.argv))
+    .scriptName("snosay")
+    .string("voice")
+    .describe("voice", "voice engine, such as: Microsoft David Desktop")
+    .number("speed")
+    .describe("speed", "voice speed, base is 1, higher faster")
+    .boolean("list")
+    .describe("list", "get voice engine list")
+    .string("output")
+    .describe("output", "output file ")
+    // examples
+    .example("$0", "hello, world")
+    .example("$0", "--list")
+    .example("$0", '--voice "Microsoft David Desktop" --speed 0.5 hello, world')
+    .example("$0", '--voice "Microsoft Zira Desktop" --speed 1.5 hello, world')
+    .example("$0", '--voice "Microsoft Zira Desktop" --speed 1.5 --output ./')
+    // .string("domain")
+    .alias("h", "help")
+    .alias("l", "list")
+    .alias("v", "voice")
+    .alias("s", "speed")
+    .alias("o", "output").argv;
 
-const params = { text: argv._.join(" "), ...argv };
-await cli(params);
-
-async function cli({ text = "", voice = "", speed = 1, output = "", list = false }) {
-  if (list) {
-    const voicelist = await getInstalledVoices();
-    console.log(voicelist.join("\n"));
-    return;
-  }
-  if (output) {
-    const path = resolve(output);
-    if (text) {
-      await exportFile(text, voice, speed, path);
-      console.log(path);
-      return;
-    }
-    const input: string = await new Promise((resolve, reject) => {
-      const lines = [] as string[];
-      const rl = readline.createInterface({ input: stdin, output: stdout });
-      rl.on("line", (line) => lines.push(line));
-      rl.on("close", () => resolve(lines.join("\n")));
-    });
-    await exportFile(input, voice, speed, path);
-    console.log(path);
-    return;
-  }
-
-  if (!text) {
-    console.warn("snosay - REPL Mode: speak the lines you just type.");
-    const Locker = ({ locked = 0, pending = [] as any } = {}) => ({
-      lock: () =>
-        new Promise((resolve) =>
-          !locked ? resolve((locked = 1)) : (locked = pending.push(resolve)),
-        ),
-      unlock: () => {
-        pending.length && pending.shift()?.();
-        locked = pending.length;
-      },
-    });
-    const { lock, unlock } = Locker();
-    const rl = readline.createInterface({ input: stdin, output: stdout });
-    const lines = [] as string[];
-    const push = async (line: string) => {
-      lines.push(line);
-      // speakers
-      await lock()
-        .then(async () => {
-          const text = lines.splice(0).join("\n");
-          !rl.terminal && console.log(text);
-          if (text) {
-            await speak(text, voice, speed);
-          }
-        })
-        .finally(unlock);
-    };
-    rl.on("line", async (line) => await push(line));
-    rl.on("close", async () => await lock() /* done */);
-
-    return;
-  }
-  console.log(text);
-  await speak(text, voice, speed);
-}
+  const params = { text: argv._.join(" "), ...argv };
+  await snosay(params);
+})();
--- packages/snosay/src/index.ts
+++ packages/snosay/src/index.ts
@@ -1,4 +1,77 @@
+import path from "path";
+import { stdin, stdout } from "process";
+import * as readline from "readline";
 import { default as say } from "say";
+
+/**
+ * @author Snowstar Miao <snomiao@gmail.com>
+ */
+export default async function snosay({
+  text = "",
+  voice = "",
+  speed = 1,
+  output = "",
+  list = false,
+}) {
+  if (list) {
+    const voicelist = await getInstalledVoices();
+    console.log(voicelist.join("\n"));
+    return;
+  }
+  if (output) {
+    const outfile = path.resolve(output);
+    if (text) {
+      await exportFile(text, voice, speed, outfile);
+      console.log(outfile);
+      return;
+    }
+    const input: string = await new Promise((resolve, reject) => {
+      const lines = [] as string[];
+      const rl = readline.createInterface({ input: stdin, output: stdout });
+      rl.on("line", (line) => lines.push(line));
+      rl.on("close", () => resolve(lines.join("\n")));
+    });
+    await exportFile(input, voice, speed, outfile);
+    console.log(outfile);
+    return;
+  }
+
+  if (!text) {
+    console.warn("snosay - REPL Mode: speak the lines you just type.");
+    const Locker = ({ locked = 0, pending = [] as any } = {}) => ({
+      lock: () =>
+        new Promise((resolve) =>
+          !locked ? resolve((locked = 1)) : (locked = pending.push(resolve)),
+        ),
+      unlock: () => {
+        pending.length && pending.shift()?.();
+        locked = pending.length;
+      },
+    });
+    const { lock, unlock } = Locker();
+    const rl = readline.createInterface({ input: stdin, output: stdout });
+    const lines = [] as string[];
+    const push = async (line: string) => {
+      lines.push(line);
+      // speakers
+      await lock()
+        .then(async () => {
+          const text = lines.splice(0).join("\n");
+          !rl.terminal && console.log(text);
+          if (text) {
+            await speak(text, voice, speed);
+          }
+        })
+        .finally(unlock);
+    };
+    rl.on("line", async (line) => await push(line));
+    rl.on("close", async () => await lock() /* done */);
+
+    return;
+  }
+  console.log(text);
+  await speak(text, voice, speed);
+}
 
 /**
  * use (say as any) because index.d.ts of say.js seems to be wrong
@@ -9,25 +82,29 @@ const callbacker =
   (err: string, result: T) =>
     err ? reject(err) : resolve(result);
 
-export const speak = (text: string, voice?: string, speed?: number): Promise<void> =>
-  new Promise((resolve, reject) =>
+export function speak(text: string, voice?: string, speed?: number): Promise<void> {
+  return new Promise((resolve, reject) =>
     (say as any).speak(text, voice, speed, callbacker<void>(resolve, reject)),
   );
+}
 
-export const stop = (): Promise<void> =>
-  new Promise((resolve, reject) => (say as any).stop(callbacker<void>(resolve, reject)));
+export function stop(): Promise<void> {
+  return new Promise((resolve, reject) => (say as any).stop(callbacker<void>(resolve, reject)));
+}
 
-export const getInstalledVoices = (): Promise<string[]> =>
-  new Promise((resolve, reject) =>
+export function getInstalledVoices(): Promise<string[]> {
+  return new Promise((resolve, reject) =>
     (say as any).getInstalledVoices(callbacker<string[]>(resolve, reject)),
   );
+}
 
-export const exportFile = (
+export function exportFile(
   text: string,
   voice?: string,
   speed?: number,
   filePath?: string,
-): Promise<void> =>
-  new Promise((resolve, reject) =>
+): Promise<void> {
+  return new Promise((resolve, reject) =>
     (say as any).export(text, voice, speed, filePath, callbacker<void>(resolve, reject)),
   );
+}
