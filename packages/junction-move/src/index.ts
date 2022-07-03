96c15cc23809851c6013a91223b774c49e8a02bc
snomiao <snomiao@gmail.com>
2022-07-03 15:57:05 +0800

    fix(junction-move): use edaf

--- packages/junction-move/src/index.ts
+++ packages/junction-move/src/index.ts
@@ -1,13 +1,10 @@
 /**
  * Copyright © 2020 snomiao@gmail.com
  */
-import { resolve } from "path";
 import snorun from "snorun";
-export default junctionMove;
-async function junctionMove(source: string, target: string) {
+export default async function junctionMove(source: string, target: string) {
   await snorun(`chcp 65001`);
   if (!(source && target)) throw new Error("empty input or output");
-  resolve();
   (true &&
     (await snorun(`robocopy "${source}" "${target}" /MOVE /E`)) &&
     (await snorun(`mklink /J "${source}" "${target}"`))) ||
