5414ec591a04109699913991ee2441c4b9cc75f7
snomiao <snomiao@gmail.com>
2022-07-07 00:09:25 +0800

    fix(snorun): okr

--- packages/snorun/src/index.ts
+++ packages/snorun/src/index.ts
@@ -18,21 +18,21 @@ import { exec } from "child_process";
 export default function snorun(cmd: string | string[], { echo = true, echoPrefix = "> " } = {}) {
   const execCommand = [cmd].flat().join(" ");
   if (echo) console.log((echoPrefix || "") + execCommand);
-  const { promise: succeedp, resolve: succ } = usePromise<boolean>();
-  const { promise: codep, resolve: code } = usePromise<number>();
-  const { promise: stdoutp, resolve: out } = usePromise<string>();
-  const { promise: stderrp, resolve: err } = usePromise<string>();
+  const { promise: ok, resolve: okr } = usePromise<boolean>();
+  const { promise: code, resolve: coder } = usePromise<number>();
+  const { promise: stdout, resolve: outr } = usePromise<string>();
+  const { promise: stderr, resolve: errr } = usePromise<string>();
   // todo: keep colors
   const p = exec(execCommand, async (error, stdout, stderr) => {
-    err(stderr.trimEnd()), out(stdout.trimEnd());
-    error.code ? succ(false) : succ(true);
-    code(error?.code || 0);
+    errr(stderr.trimEnd()), outr(stdout.trimEnd());
+    error ? okr(false) : okr(true);
+    coder(error?.code || 0);
   });
   p.stderr.pipe(process.stderr);
   p.stdout.pipe(process.stdout);
-  // process.stdin.read && process.stdin.pipe(p.stdin);
-  const subPromises = { ok: succeedp, stdout: stdoutp, stderr: stderrp };
-  return { catch: succeedp.catch, finally: succeedp.finally, then: succeedp.then, ...subPromises };
+  const subPromises = { ok, stdout, stderr, code };
+  const retype = { ...ok, ...subPromises };
+  return Object.assign(ok, subPromises) as typeof retype;
 }
 function usePromise<T>() {
   const s: any = {};
