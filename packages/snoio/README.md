e0ef971c4ad499f0fbd31856321f8cb3c3b56b4f
snomiao <snomiao@gmail.com>
2022-09-08 12:59:40 +0800

    fix: drafting snoio utils

--- packages/snoio/README.md
+++ packages/snoio/README.md
@@ -2,5 +2,7 @@
 
 ## Feat
 
-pipe
-files 
+Basic Encoding & Decoding (UTF8 and Base64)
+FileSystem Control (Read, Write)
+Stream Collect (Merge two world: Promises and Stream)
+
--- packages/snoio/src/index.ts
+++ packages/snoio/src/index.ts
@@ -1,7 +1,27 @@
+import { readFile, writeFile } from "fs/promises";
 /**
- * 
+ *
  * @author: snomiao <snomiao@gmail.com>
  */
-export default async function snoio() {
-    console.log('hello')
-};
+export default async function snoio(filename: string) {
+  console.log("hello");
+  return {
+    fileTextRead: () => fileTextRead(filename),
+    fileRead: () => fileRead(filename),
+    fileWrite: (content: any) => fileWrite(filename, content),
+  };
+}
+
+export async function fileTextRead(filename: string) {
+  return (
+    (await readFile(filename, { encoding: "utf8" }))
+      // handle utf8 with bom
+      ?.replace(/^\ufeff/, "")
+  );
+}
+export async function fileRead(filename: string) {
+  return await readFile(filename);
+}
+export async function fileWrite(filename: string, content: any) {
+  return await writeFile(filename, content);
+}
