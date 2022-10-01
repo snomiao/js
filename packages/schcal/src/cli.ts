9512820a5652360f1b888bc13f91b86f430fe1b8
snomiao <snomiao@gmail.com>
2022-10-01 16:15:36 +0800

    fix(encoding):

--- packages/schcal/src/cli.ts
+++ packages/schcal/src/cli.ts
@@ -17,7 +17,8 @@ schcal();
 export default async function schcal() {
   await snorun(`chcp 65001`);
   // READING PARAMS
-  const argv = await yargs(hideBin(process.argv))
+  const argv = await // .env()
+  yargs(hideBin(process.argv))
     .usage("Usage: schcal [options] [...ICS_URLS]")
     .alias("s", "startup")
     .alias("c", "config")
@@ -28,13 +29,12 @@ export default async function schcal() {
     .alias("v", "version")
     .alias("h", "help")
     .example(
-      "$0 https://calendar.google.com/calendar/ical/xxxxxxxxxxxxxxxxxxx/private-cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/basic.ics",
+      "$0 https://calendar.google.com/calendar/ical" +
+        "/xxxxxxxxxxxxxxxxxxx/private-cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/basic.ics",
       "load this ics to schtasks",
     )
     .example("schcal -c config.yaml", "run with config.yaml")
-    .epilog("Copyright (c) 2020 snomiao@gmail.com")
-    // .env()
-    .argv;
+    .epilog("Copyright (c) 2020 snomiao@gmail.com").argv;
   if (argv.startup) await snorun(`./add-to-schtasks.bat`);
   await snorun(`title SSAC - READING config`);
   const config = await readConfig(argv);
