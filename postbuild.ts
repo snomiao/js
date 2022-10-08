74f406b670399551ad1ca13ec740134fd74a5681
snomiao <snomiao@gmail.com>
2022-10-08 12:22:56 +0000

    feat(pkg): snobuild v3

---
+++ postbuild.ts
@@ -0,0 +1,6 @@
+import fsp from 'fs/promises'
+import { cmds } from './src/index'
+await Promise.all(cmds.map(cmd =>
+    fsp.copyFile('./dist/cli.mjs', './dist/cli-' + cmd + '.mjs').then(e => console.log('cli-' + cmd + ' copied'))
+))
+console.log('post build done');
