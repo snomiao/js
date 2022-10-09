80a95ec38a70d2c1b92af5597254d72be64bc22d
snomiao <snomiao@gmail.com>
2022-10-09 11:24:24 +0800

    chore(lint):

--- postbuild.ts
+++ postbuild.ts
@@ -1,6 +1,10 @@
-import fsp from 'fs/promises'
-import { cmds } from './src/index'
-await Promise.all(cmds.map(cmd =>
-    fsp.copyFile('./dist/cli.mjs', './dist/cli-' + cmd + '.mjs').then(e => console.log('cli-' + cmd + ' copied'))
-))
-console.log('post build done');
+import fsp from "fs/promises";
+import { cmds } from "./src/index";
+await Promise.all(
+  cmds.map((cmd) =>
+    fsp
+      .copyFile("./dist/cli.mjs", `./dist/cli-${  cmd  }.mjs`)
+      .then((e) => console.log(`cli-${  cmd  } copied`)),
+  ),
+);
+console.log("post build done");
