7006095105246ce096e02073bed225c4bc8aa6b8
snomiao <snomiao@gmail.com>
2022-10-11 17:21:17 +0800

    feat(packages): init

---
+++ packages/snocommit/README.md
@@ -0,0 +1,7 @@
+# snocommit
+
+
+// styles (part) description
+// docs (part) description
+// fix (part) description
+// feat (part) description
--- packages/snocommit/postbuild.ts
+++ packages/snocommit/postbuild.ts
@@ -1,10 +1,15 @@
+import { execAndLog } from "async-exec";
 import fsp from "fs/promises";
-import { cmds } from "./src/index";
-await Promise.all(
-  cmds.map((cmd) =>
-    fsp
-      .copyFile("./dist/cli.mjs", `./dist/cli-${  cmd  }.mjs`)
-      .then((e) => console.log(`cli-${  cmd  } copied`)),
-  ),
-);
+import "p-map";
+import pMap from "p-map";
+import { types } from "./src/index";
+
+await pMap(types, async (type) => {
+  const targetFile = `dist/cli-${type}.mjs`;
+  const r = await execAndLog(`npm pkg set bin.${type}="${targetFile}"`);
+  return await fsp
+    .copyFile("dist/cli.mjs", targetFile)
+    .then((e) => console.log(`cli-${type} seted up`));
+});
+
 console.log("post build done");
