9bdb1895ad50103ed69e0af87042ee6a30c97376
snomiao <snomiao@gmail.com>
2022-10-11 17:21:17 +0800

    feat(packages): init

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 
-import { cmds } from "./index";
-const [node, js, ...rest] = process.argv;
-const cmdFromJS = cmds.find((e) => e === js.match(/(?:cli-|bin\/)(.*?)(?:\.[mc]?[tj]s)?$/)?.[1]);
-const { cmd, part, descs } = cmdFromJS
-  ? (() => {
-      const [part, ...descs] = rest;
-      return { cmd: cmdFromJS, part, descs };
-    })()
-  : (() => {
-      const [cmd, part, ...descs] = rest;
-      return { cmd, part, descs };
-    })();
-const desc = descs.join(" ");
-const valid = cmds.includes(cmd as any);
-console.log(process.argv);
-console.log([node, js, cmd, part, desc], { valid });
+import snocommit, { types } from "./index";
+
+cli().then();
+async function cli() {
+  const [node, js, ...rest] = process.argv;
+  const cmdFromJS = types.find((e) => e === js.match(/(?:cli-|bin\/)(.*?)(?:\.[mc]?[tj]s)?$/)?.[1]);
+  const { type, part, descs } = cmdFromJS
+    ? (() => {
+        const [part, ...descs] = rest;
+        return { type: cmdFromJS, part, descs };
+      })()
+    : (() => {
+        const [_cmd, part, ...descs] = rest;
+        return { type: types.find((e) => e === _cmd), part, descs };
+      })();
+  const desc = descs.join(" ");
+  if (!type) throw new Error(`commit type not found: ${rest[0]}`);
+  await snocommit({ type, part, desc });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-// style (part) description
-// docs (part) description
-// fix (part) description
-// feat (part) description
+import path from "path";
+import snorun from "snorun";
 // breaking (part) description
-export const cmds = ["fix", "style", "feat", "breaking", "docs"] as const;
-type CMD = typeof cmds[number];
+export const types = ["fix", "styles", "feat", "breaking", "docs"] as const;
+type Type = typeof types[number];
 type PART = "-" | "." | string;
-const cmdActions: Record<CMD, (part: PART, desc: string) => Promise<void> | void> = {
-  breaking: () => null,
-  docs: () => null,
-  feat: () => null,
-  fix: () => null,
-  style: () => null,
+const cmdActions: Record<Type, (part: PART, desc: string) => Promise<any> | any> = {
+  breaking: (part, desc) => "npm version major && " && "",
+  feat: (part, desc) => "npm version minor && " && "",
+  styles: (part, desc) => "npm version patch && " && "",
+  docs: (part, desc) => "",
+  fix: (part, desc) => "",
 };
-export default function snocommit(cmd: CMD, part: PART, desc: string) {
-  cmdActions[cmd];
+type snoCommitOptions = {
+  type: Type;
+  part: PART;
+  desc: string;
+};
+
+export default async function snocommit({ type, part, desc }: snoCommitOptions) {
+  if (part === ".") {
+    part = path.parse(path.parse(process.cwd()).dir).name;
+  }
+  const action = cmdActions[type];
+  if (!action) throw new Error(`no such cmd: ${type}`);
+  const cmdPrefix = action(part, desc);
+
+  const valid = Boolean(cmdActions[type]);
+  if (valid) {
+    const quoted = (e: string) => (e ? `(${e})` : "");
+    const msg = `${type}${quoted(part)}: ${desc}`;
+    const cmd = `${cmdPrefix}git add . && git commit -m "${msg}" && git pull && git push`;
+    // console.log(chalk.blue(`> ${cmd}`));
+    await snorun(cmd);
+  }
 }
