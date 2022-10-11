2dc9915a92506942b2e16c2e0a450cb196a9afc1
snomiao <snomiao@gmail.com>
2022-10-11 22:32:06 +0800

    docs(top-level-git): example

---
+++ packages/top-level-git/README.md
@@ -0,0 +1,10 @@
+# top-level-git
+
+A dead simple wrapper of `git rev-parse --show-toplevel`
+
+## Usage
+
+```javascript
+import topLevelGit from "top-level-git";
+await topLevelGit(); // => "/path/to/your/top/repo"
+```
--- packages/top-level-git/src/index.ts
+++ packages/top-level-git/src/index.ts
@@ -1,12 +1,8 @@
 import execSh from "exec-sh";
-import { parse, resolve } from "path";
-
-export async function topLevelGit() {
-  const top = await execSh
+export default async function topLevelGit() {
+  const topdir = await execSh
     .promise("git rev-parse --show-toplevel", true)
     .then((e) => e.stdout.trim());
-  if (!top) throw new Error("fail to get git rev-parse --show-toplevel");
-  const repodir = resolve(top); /* process.cwd() */
-  const repodirname = parse(repodir).name;
-  return { repodir, top, repodirname };
+  if (!topdir) throw new Error("fail to get git rev-parse --show-toplevel");
+  return topdir;
 }
