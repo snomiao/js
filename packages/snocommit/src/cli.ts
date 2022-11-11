fd0778174a7b667599b18cde7b6201f82f4454da
snomiao <snomiao@gmail.com>
2022-11-11 13:20:05 +0900

    fix: scope

--- packages/snocommit/src/cli.ts
+++ packages/snocommit/src/cli.ts
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
-import snocommit, { types } from "./index";
+import commitTypes from "./commitTypes";
+import snocommit from "./index";
 
 cli().then();
 
 async function cli() {
   const [node, js, ...rest] = process.argv;
-  const cmdFromJS = types.find((e) => e === js.match(/(?:cli-|bin\/)(.*?)(?:\.[mc]?[tj]s)?$/)?.[1]);
-  const { type, part, descs } = cmdFromJS
+  const cmdFromJS = commitTypes.find(
+    (e) => e === js.match(/(?:cli-|bin\/)(.*?)(?:\.[mc]?[tj]s)?$/)?.[1],
+  );
+  const { type, scope, descs } = cmdFromJS
     ? (() => {
-        const [part, ...descs] = rest;
-        return { type: cmdFromJS, part, descs };
+        const [scope, ...descs] = rest;
+        return { type: cmdFromJS, scope, descs };
       })()
     : (() => {
-        const [_cmd, part, ...descs] = rest;
-        return { type: types.find((e) => e === _cmd), part, descs };
+        const [_cmd, scope, ...descs] = rest;
+        return { type: commitTypes.find((e) => e === _cmd), scope, descs };
       })();
   const desc = descs.join(" ");
   if (!type) throw new Error(`commit type not found: ${rest[0]}`);
-  await snocommit({ type, part, desc });
+  await snocommit({ type, scope, desc });
 }
---
+++ packages/snocommit/src/commitTypes.ts
@@ -0,0 +1,3 @@
+// breaking (scope) descriptions
+const commitTypes = ["fix", "styles", "feat", "breaking", "docs", "chore", "refactor"] as const;
+export default commitTypes;
