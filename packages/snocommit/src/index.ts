50bf67515185f961374834dc46e804b728c22f42
snomiao <snomiao@gmail.com>
2022-11-17 10:22:23 +0900

    fix(src): free from versioning Actions

--- packages/snocommit/src/index.ts
+++ packages/snocommit/src/index.ts
@@ -4,24 +4,25 @@ import maybeQuoted from "./maybeQuoted";
 import scopeParse from "./scopeParse";
 type Type = typeof commitTypes[number];
 type SCOPE = "-" | "." | "@" | ":" | string;
-const cmdActions: Record<Type, (scope: SCOPE, desc: string) => Promise<any> | any> = {
-  breaking: (scope, desc) =>
-    `(npm version major --no-workspaces-update${
-      " || echo [WARN] error throws while version bump)" && ")"
-    }`,
-  feat: (scope, desc) =>
-    `(npm version minor --no-workspaces-update${
-      " || echo [WARN] error throws while version bump)" && ")"
-    }`,
-  fix: (scope, desc) =>
-    `(npm version patch --no-workspaces-update${
-      " || echo [WARN] error throws while version bump)" && ")"
-    }`,
-  refactor: (scope, desc) => "",
-  chore: (scope, desc) => "",
-  docs: (scope, desc) => "",
-  styles: (scope, desc) => "",
-};
+
+// const cmdActions: Record<Type, (scope: SCOPE, desc: string) => Promise<any> | any> = {
+//   breaking: (scope, desc) =>
+//     `(npm version major --no-workspaces-update${
+//       " || echo [WARN] error throws while version bump)" && ")"
+//     }`,
+//   feat: (scope, desc) =>
+//     `(npm version minor --no-workspaces-update${
+//       " || echo [WARN] error throws while version bump)" && ")"
+//     }`,
+//   fix: (scope, desc) =>
+//     `(npm version patch --no-workspaces-update${
+//       " || echo [WARN] error throws while version bump)" && ")"
+//     }`,
+//   refactor: (scope, desc) => "",
+//   chore: (scope, desc) => "",
+//   docs: (scope, desc) => "",
+//   styles: (scope, desc) => "",
+// };
 type snoCommitOptions = {
   type: Type;
   scope: SCOPE;
@@ -31,20 +32,21 @@ type snoCommitOptions = {
 export default async function snocommit({ type, scope, desc }: snoCommitOptions) {
   if (!desc) throw new Error("missing desc");
   const parsedPart = await scopeParse(scope);
-  const versioningAction = cmdActions[type];
-  if (!versioningAction) throw new Error(`no such cmd: ${type}`);
-  const versioningCmd = await versioningAction(parsedPart, desc);
-  const valid = Boolean(cmdActions[type]);
-  if (valid) {
-    const msg = `${type}${maybeQuoted(parsedPart)}: ${desc}`;
-    const gitsync_cmd = `git pull && git push --follow-tags`;
+  // const versioningAction = cmdActions[type];
+  // if (!versioningAction) throw new Error(`no such cmd: ${type}`);
+  // const versioningCmd = await versioningAction(parsedPart, desc);
+  // const valid = Boolean(cmdActions[type]);
+  // if (valid) {
+  const msg = `${type}${maybeQuoted(parsedPart)}: ${desc}`;
+  const gitsync_cmd = `git pull && git push --follow-tags`;
 
-    true &&
-      (await snorun(`git add . && git commit -m "${msg}"`)) &&
-      (!versioningCmd
-        ? true
-        : (await snorun(versioningCmd)) &&
-          ((await snorun(`(git add . && git commit -m "${msg}")`)) || true)) &&
-      (await snorun(gitsync_cmd));
-  }
+  true &&
+    (await snorun(`git add . && git commit -m "${msg}"`)) &&
+    // (!versioningCmd
+    //   ? true
+    //   : (await snorun(versioningCmd)) &&
+    ((await snorun(`(git add . && git commit -m "${msg}")`)) || true) &&
+    // )
+    (await snorun(gitsync_cmd));
+  // }
 }
