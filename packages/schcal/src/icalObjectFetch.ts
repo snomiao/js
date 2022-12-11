e2373bf30ec5cbb3a32e4a9166eff6103c13c28a
snomiao <snomiao@gmail.com>
2022-12-11 11:01:44 +0900

    fix(schcal): proxy-agent without proxy

--- packages/schcal/src/icalObjectFetch.ts
+++ packages/schcal/src/icalObjectFetch.ts
@@ -2,9 +2,7 @@ import ical from "ical";
 import fetch from "node-fetch";
 import tunnel from "tunnel";
 export default async function icalObjectFetch(url: string) {
-  const { hostname, port } = new URL(process.env.HTTP_PROXY);
-  const proxy = { host: String(hostname), port: Number(port) };
-  const agent = tunnel.httpsOverHttp({ proxy });
+  const agent = proxyAgent();
   const r = await fetch(url, { ...(agent && { agent }) }).catch((e) => {
     throw new Error(
       `Fetch on URL: ${url} \nError details: ${e}\n\n` +
@@ -13,4 +11,12 @@ export default async function icalObjectFetch(url: string) {
   });
   const s = await r.text();
   return ical.parseICS(s);
+}
+function proxyAgent() {
+  const HTTP_PROXY = process.env.HTTP_PROXY;
+  if(!HTTP_PROXY) return;
+  const { hostname, port } = new URL(HTTP_PROXY);
+  const proxy = { host: String(hostname), port: Number(port) };
+  const agent = tunnel.httpsOverHttp({ proxy });
+  return agent;
 }
