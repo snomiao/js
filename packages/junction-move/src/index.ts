be1c9e0e332448c9e7ed55069415718de6077c8b
snomiao <snomiao@gmail.com>
2023-01-08 02:42:55 +0900

    fix(src): deter

--- packages/junction-move/src/index.ts
+++ packages/junction-move/src/index.ts
@@ -5,8 +5,7 @@ import snorun from "snorun";
 export default async function junctionMove(source: string, target: string) {
   await snorun(`chcp 65001`);
   if (!(source && target)) throw new Error("empty input or output");
-  (true &&
-    (await snorun(`robocopy "${source}" "${target}" /MOVE /E`)) &&
-    (await snorun(`mklink /J "${source}" "${target}"`))) ||
-    console.log("[JunctionMove] Fail to finish the move");
+  await snorun(`robocopy "${source}" "${target}" /MOVE /E`);
+  await snorun(`mklink /J "${source}" "${target}"`);
+  console.log("[JunctionMove] Finished, please check the log result to determine it's succeed");
 }
