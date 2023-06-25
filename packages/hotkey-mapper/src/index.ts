88f213340fb47bb028892d4bcd4fdf8243953a50
snomiao <snomiao@gmail.com>
2023-06-25 16:16:22 +0900

    fix(hotkey-mapper): lower
    lower

--- packages/hotkey-mapper/src/index.ts
+++ packages/hotkey-mapper/src/index.ts
@@ -3,16 +3,17 @@ type handler = (e: KeyboardEvent) => void;
 /**
  * @reference view js keycode here: https://www.toptal.com/developers/keycode
  * @example
-   hotkeyMapper(
-    {
-      "alt+1": () => openLinkByCount(2 ** 1),
-      "alt+2": () => openLinkByCount(2 ** 2),
-      "alt+3": () => openLinkByCount(2 ** 3),
-      "shift+alt+1": () => tryCopyLinkByCount(2 ** 1),
-      "shift+alt+2": () => tryCopyLinkByCount(2 ** 2),
-      "shift+alt+3": () => tryCopyLinkByCount(2 ** 3),
-    },
-    {capture: true})
+ * hotkeyMapper(
+ *   {
+ *     "alt+1": () => openLinkByCount(2 ** 1),
+ *     "alt+2": () => openLinkByCount(2 ** 2),
+ *     "alt+3": () => openLinkByCount(2 ** 3),
+ *     "shift+alt+1": () => tryCopyLinkByCount(2 ** 1),
+ *     "shift+alt+2": () => tryCopyLinkByCount(2 ** 2),
+ *     "shift+alt+3": () => tryCopyLinkByCount(2 ** 3),
+ *   },
+ *   {capture: true}
+ * )
  */
 export default function hotkeyMapper<K extends keyof GlobalEventHandlersEventMap>(
   mapping: Record<string, handler>,
@@ -21,7 +22,7 @@ export default function hotkeyMapper<K extends keyof GlobalEventHandlersEventMap
   const handler = (event: KeyboardEvent) => {
     const key = event.key.toLowerCase();
     const code = event.code.toLowerCase();
-    const simp = code.replace(/^(?:Key|Digit|Numpad)/, "");
+    const simp = event.code.replace(/^(?:Key|Digit|Numpad)/, "").toLowerCase();
     const map = new Proxy(event, {
       get: (target, p: string) =>
         Boolean(
@@ -38,9 +39,11 @@ export default function hotkeyMapper<K extends keyof GlobalEventHandlersEventMap
         .replace(/win|command|search/, "meta")
         .replace(/control/, "ctrl")
         .split("+")
+        .map((key) => key.toLowerCase().trim())
         .map((k, i) => [k, i >= 4 === (map as any)[`${k}Key`]]);
       if (!Object.entries(Object.fromEntries(conds)).every(([, ok]) => ok)) return;
-      event.stopPropagation(), event.preventDefault();
+      event.stopPropagation();
+      event.preventDefault();
       return fn(event);
     }, mapping);
   };
